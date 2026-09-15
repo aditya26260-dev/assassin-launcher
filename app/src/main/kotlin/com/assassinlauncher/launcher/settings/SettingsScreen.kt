e7feecddf6c29dfb2cd5b48e9bb7e152e5de518a
aditya26260 <aditya26260@gmail.com>
2026-09-15 17:44:24 +0530

    Opt in to ExperimentalMaterial3Api for the renderer dropdown

--- app/src/main/kotlin/com/assassinlauncher/launcher/settings/SettingsScreen.kt
+++ app/src/main/kotlin/com/assassinlauncher/launcher/settings/SettingsScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.HorizontalDivider
@@ -244,6 +245,7 @@ fun SettingsScreen(device: DeviceProfile, onBack: () -> Unit) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun DefaultRendererDropdown(
     selected: ManualRendererOverride?,
